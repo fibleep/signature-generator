34e216147383ec6f7849f2b489f587d1a321f64b
fibleep <filipn924@gmail.com>
2025-06-22 17:00:57 +0200

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://fibleep.github.io",
+	base: "/signature-generator",
 	integrations: [react()],
 });
